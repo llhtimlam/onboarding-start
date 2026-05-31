374616c08c35f22f75d90fa6748277ac318b43cc
llhtimlam <llhtimlam@gmail.com>
2026-05-31 02:57:14 -0400

    Completion

--- src/project.v
+++ src/project.v
@@ -209,7 +209,7 @@ module tt_um_uwasic_onboarding_llhtimlam #(
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwm_duty_cycle)
+    .pwm_duty_cycle(pwm_duty_cycle),
     .out({uio_out, uo_out})
   );
 
